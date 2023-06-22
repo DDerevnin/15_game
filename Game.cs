7592ca311416da4111557d8cfbaf9639e1a90ba9
Reidego <yep30042003@gmail.com>
2023-06-22 05:27:54 +0500

    Добавьте файлы проекта.

---
+++ Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_game
+{
+    class Game
+    {
+        int size;
+        int[,] map;
+
+        public Game (int size)
+        {
+            if (size < 2) size = 2;
+            if (size > 5) size = 5;
+            this.size = size;
+            map = new int [size, size];
+        }
+    }
+}
